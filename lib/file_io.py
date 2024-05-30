b685b2d7ec1874a98d2196479c242bbb6a57c2ac
johney <johneyburke@gmail.com>
2024-05-30 10:18:53 -0600

    fileIO

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name, mode ='w',encoding='utf-8') as log_file:
+        log_file.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name, mode='a', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name, encoding="utf-8") as text_file:
+        print(text_file.read())
